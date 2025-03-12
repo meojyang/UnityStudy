e5719cd2d9e3c493397eaaf0f16abd71fd3c1663
Guy <khk3482@gmail.com>
2025-03-12 13:39:52 +0900

    Updated

--- 1945/Assets/Scripts/Player.cs
+++ 1945/Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
     private Vector2 minBounds;
     private Vector2 maxBounds;
 
+    Animator ani; //애니메이터를 가져올 함수
+
 
     void Start()
     {
@@ -16,21 +18,41 @@ public class Player : MonoBehaviour
 
         minBounds = new Vector2(bottomLeft.x, bottomLeft.y);
         maxBounds = new Vector2(topRight.x, topRight.y);
+
+
+        ani = GetComponent<Animator>();
     }
     
     void Update()
     {
+        //방향키에 따른 움직임
         float moveX = moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
         float moveY = moveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
 
-        transform.Translate(moveX, moveY, 0);
-
         Vector3 newPosition = transform.position + new Vector3(moveX, moveY, 0);
 
-        // 경계를 벗어나지 않도록 위치 제한
+
         newPosition.x = Mathf.Clamp(newPosition.x, minBounds.x, maxBounds.x);
         newPosition.y = Mathf.Clamp(newPosition.y, minBounds.y, maxBounds.y);
 
         transform.position = newPosition;
+
+        if (Input.GetAxis("Horizontal") <= -0.5f)        
+            ani.SetBool("left", true);        
+        else        
+            ani.SetBool("left", false);
+        
+
+        if (Input.GetAxis("Horizontal") >= 0.5f)
+            ani.SetBool("right", true);        
+        else       
+            ani.SetBool("right", false);
+        
+
+
+
+        transform.Translate(moveX, moveY, 0);
+
+        
     }
 }
