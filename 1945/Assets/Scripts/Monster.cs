77a35cc210896011802ec86135ebb159f8d9ea84
Guy <khk3482@gmail.com>
2025-03-13 08:56:48 +0900

    Updated

---
+++ 1945/Assets/Scripts/Monster.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+
+    public float moveSpeed = 3f;
+
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
--- 1945/Assets/Scripts/Player.cs
+++ 1945/Assets/Scripts/Player.cs
@@ -2,24 +2,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-
-    private Vector2 minBounds;
-    private Vector2 maxBounds;
+    public float moveSpeed = 5f;     
 
     Animator ani; //애니메이터를 가져올 함수
 
 
     void Start()
-    {
-        Camera cam = Camera.main;
-        Vector3 bottomLeft = cam.ViewportToWorldPoint(new Vector3(0, 0, 0));
-        Vector3 topRight = cam.ViewportToWorldPoint(new Vector3(1, 1, 0));
-
-        minBounds = new Vector2(bottomLeft.x, bottomLeft.y);
-        maxBounds = new Vector2(topRight.x, topRight.y);
-
-
+    {        
         ani = GetComponent<Animator>();
     }
     
@@ -29,27 +18,25 @@ public class Player : MonoBehaviour
         float moveX = moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
         float moveY = moveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
 
-        Vector3 newPosition = transform.position + new Vector3(moveX, moveY, 0);
+        
 
-
-        newPosition.x = Mathf.Clamp(newPosition.x, minBounds.x, maxBounds.x);
-        newPosition.y = Mathf.Clamp(newPosition.y, minBounds.y, maxBounds.y);
-
-        transform.position = newPosition;
-
-        if (Input.GetAxis("Horizontal") <= -0.5f)        
+        if (Input.GetAxis("Horizontal") <= -0.1f)        
             ani.SetBool("left", true);        
         else        
             ani.SetBool("left", false);
         
 
-        if (Input.GetAxis("Horizontal") >= 0.5f)
+        if (Input.GetAxis("Horizontal") >= 0.1f)
             ani.SetBool("right", true);        
         else       
             ani.SetBool("right", false);
+
+        if (Input.GetAxis("Vertical") >= 0.1f)
+            ani.SetBool("up", true);
+        else
+            ani.SetBool("up", false);
+
         
-
-
 
         transform.Translate(moveX, moveY, 0);
 
